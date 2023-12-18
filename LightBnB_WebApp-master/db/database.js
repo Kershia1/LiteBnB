506ef3affc04347594166255251080c48cc69c2c
Kershia1 <insatiable.meerkat@gmail.com>
2023-12-18 03:40:13 +0000

    refactored getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -60,8 +60,8 @@ const addUser = function (user) {
 
   return pool 
   .query(`INSERT INTO users (name, email, password) 
-  VALUES ($1,$2,$3) 
-  RETURNING *;`, [user.name, user.email, user.password]) //THIS IS A CLAUSE TO RETURN EXECUTED DATA 
+    VALUES ($1,$2,$3) 
+    RETURNING *;`, [user.name, user.email, user.password]) //THIS IS A CLAUSE TO RETURN EXECUTED DATA 
   .then((results) => {
     return results.rows[0];
   })
@@ -77,8 +77,29 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+
+const getAllReservations = function(guest_id, limit = 10) {
+
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM ( SELECT DISTINCT ON (property_id) * FROM reservations
+    WHERE guest_id = $1
+    ORDER BY property_id, guest_id
+  ) AS reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY properties.title
+  LIMIT $2;`, [guest_id, limit])
+.then((results) => {
+    console.log(results.rows);
+    return results.rows.slice(0, 10);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /// Properties
@@ -90,6 +111,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
+
+  // SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+  // FROM properties
+  // LEFT JOIN property_reviews ON properties.id = property_id
+  // WHERE city LIKE '%ancouv%'
+  // GROUP BY properties.id
+  // HAVING avg(property_reviews.rating) >= 4
+  // ORDER BY cost_per_night
+  // LIMIT 10;
+
   return pool 
   .query(`SELECT * FROM properties LIMIT $1`, [limit])
   .then((result) => {
